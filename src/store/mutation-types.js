535da3a0032b3ca7df4d3d554a9c06e27fd1d7f3
Eric <657345933@qq.com>
2023-04-10 13:14:08 +0800

    Update mutation-types.js

--- src/store/mutation-types.js
+++ src/store/mutation-types.js
@@ -1,8 +1,8 @@
 //AI头像地址设置
 export const AI_HEAD_IMG_URL="https://th.bing.com/th?id=ODL.3e2fbff4543f0d3632d34be6d02adc93&w=100&h=100&c=12&pcl=faf9f7&o=6&dpr=1.5&pid=13.1"
 //用户头像地址设置
-export const USER_HEAD_IMG_URL="https://avatars.githubusercontent.com/u/40659515?v=4"
+export const USER_HEAD_IMG_URL="https://img2.doubanio.com/view/photo/raw/public/p1629476882.jpg"
 //用户名称设置
-export const USER_NAME="君尘陌"
+export const USER_NAME="大卫"
 
 
